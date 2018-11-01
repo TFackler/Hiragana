77ee0f9012dd5f72ba79cefe18a326abdcfc29b5
TFackler <tamino.fackler@student.uni-augsburg.de>
2018-11-01 21:08:16 +0100

    Actually Added MainFrame for Visualization

---
+++ src/MainFrame.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+/**
+ * Created by Tamino on 01.11.2018.
+ */
+
+
+public class MainFrame extends JFrame {
+
+
+
+    public static void main(String[]args){
+        new MainFrame();
+    }
+    
+}
+
+
